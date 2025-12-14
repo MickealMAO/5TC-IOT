013ed1b240a64093944e54d84ed9d1f61490da6d
Yizuo GUO <126800748+Gio762002@users.noreply.github.com>
2025-12-14 11:33:56 +0100

    Clean up beacon.ino by removing unused comments
    Removed commented-out code for Service UUID and advertising parameters.

--- beacon/beacon.ino
+++ beacon/beacon.ino
@@ -6,11 +6,6 @@
 // Define the name you want to broadcast
 #define DEVICE_NAME "RG_BEACON"
 
-// Define a Service UUID if you want to advertise a specific service.
-// If you only want to broadcast the device name, you can comment this out.
-// For this simple beacon, we will focus on the device name.
-// const char* SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b"; 
-
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting BLE Beacon: " DEVICE_NAME);
@@ -20,7 +15,6 @@ void setup() {
   BLEDevice::init(DEVICE_NAME); 
 
   // Set the TX power for Advertising (ADV), Scan Response (SCAN), and Default
-  // ESP_PWR_LVL_P9 is the highest level, typically +9 dBm on ESP32/ESP32-S3
   esp_err_t advResult = esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, ESP_PWR_LVL_P9);
   esp_err_t scanResult = esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_SCAN, ESP_PWR_LVL_P9);
   esp_err_t defResult = esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_DEFAULT, ESP_PWR_LVL_P9);
@@ -29,15 +23,9 @@ void setup() {
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   
   // Set the device name in the advertising data
-  // This is a common and simple way to create a beacon.
   pAdvertising->setAppearance(0x0000); // Generic appearance
   pAdvertising->setScanResponse(true); // Allow scanning for more info (e.g., name)
 
-  // Configure advertising parameters
-  // ADV_IND: Connectable undirected advertising. This means the device can be connected to.
-  // ADV_NONCONN_IND: Non-connectable undirected advertising (if you want it to ONLY be a beacon).
-  // pAdvertising->setAdvertisementType(ADV_TYPE_NONCONN_IND); 
-  
   // Set advertising intervals (in multiples of 0.625ms). 
   // 0x20 is 20ms, 0x40 is 40ms. Lower interval means more frequent, but higher power use.
   pAdvertising->setMinPreferred(0x06); // Tmin = 6 * 0.625ms = 3.75ms
